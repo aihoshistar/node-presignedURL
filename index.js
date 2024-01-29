5e5ce84e645ea288c9930bea767577abe04f47f9
aihoshistar <troublesome.dev@gmail.com>
2024-01-29 19:59:38 +0900

    add put getSignedUrl

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
+require('dotenv').config();
+
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
 const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const {
+    AWS_ACCESS_KEY,
+    AWS_SECRET_KEY,
+    AWS_S3_BUCKET,
+    AWS_REGION,
+  } = process.env;
+
+  console.log(AWS_ACCESS_KEY,
+    AWS_SECRET_KEY,
+    AWS_S3_BUCKET,
+    AWS_REGION)
+
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+    region: AWS_REGION,
+});
+  
+async function getPresignedUploadUrl(directory) {
+    const key = `${directory}/${uuid.v4()}`;
+    const url = await s3.getSignedUrl('putObject', {
+        Bucket: AWS_S3_BUCKET,
+        Key: key,
+        ContentType: 'image/*',
+        Expires: 300,
+      });
+    return url;
+  }
+
+app.get('/', async (req, res) => {
+    const presignedURL = await getPresignedUploadUrl('profile');
+  res.json({data: presignedURL})
 })
 
-app.listen(5000)
+app.listen(5555);
+console.log("http://localhost:5555");
